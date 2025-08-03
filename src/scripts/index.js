e999ebc9eed5383e4d15f7e608ce772ce56f3c4a
nickonyi <nickoluoch1130@gmail.com>
2025-08-03 14:04:11 +0300

    feat: update main menu and rules section for improved visibility and interaction; enhance styles for better layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,20 @@
-// index.js
 import "../styles/style.css";
-import { greeting } from "./game";
 
-console.log(greeting);
+const gameRulesBtn = document.querySelector("#game-rules-btn");
+const checkBtn = document.querySelector(".close-rules-btn");
+
+const toggleBoxes = () => {
+  const box1 = document.querySelector(".main-menu-tab");
+  const box2 = document.querySelector(".rules-section");
+
+  if (box1.classList.contains("visible")) {
+    box1.classList.replace("visible", "hidden");
+    box2.classList.replace("hidden", "visible");
+  } else {
+    box2.classList.replace("visible", "hidden");
+    box1.classList.replace("hidden", "visible");
+  }
+};
+
+gameRulesBtn.addEventListener("click", toggleBoxes);
+checkBtn.addEventListener("click", toggleBoxes);

4ebecc85b2b5d4c77b5ca90f2116aef1d15ae95c
nickonyi <nickoluoch1130@gmail.com>
2025-08-08 23:44:17 +0300

    feat: update scoreboard display and button IDs for improved gameplay experience

--- src/scripts/uiController.js
+++ src/scripts/uiController.js
@@ -1,15 +1,62 @@
 export const UIcontroller = () => {
   const homeScreen = document.getElementById("home-screen");
   const gameScreen = document.getElementById("game-screen");
-  const pvpButton = document.getElementById("pvp-btn");
+  const leftScoreboard = document.getElementById("left-scoreboard");
+  const rightScoreboard = document.getElementById("right-scoreboard");
 
   const showGameScreen = () => {
     homeScreen.classList.add("hidden");
     gameScreen.classList.remove("hidden");
   };
 
+  const updateScoreboard = (mode) => {
+    if (mode === "pvp") {
+      leftScoreboard.innerHTML = `
+      <div class="player-box box-shadow-black">
+        <div class="player-name">Player 1</div>
+        <div class="player-score">0</div>
+        <img src="./assets/images/player-one.svg" alt="" class="player-icon" />
+      </div>
+    `;
+
+      rightScoreboard.innerHTML = `
+      <div class="player-box box-shadow-black">
+        <div class="player-name">Player 2</div>
+        <div class="player-score">0</div>
+        <img src="./assets/images/player-two.svg" alt="" class="player-icon" />
+      </div>
+    `;
+    }
+
+    if (mode === "cpu") {
+      leftScoreboard.innerHTML = `
+      <div class="player-box box-shadow-black">
+        <div class="player-name">You</div>
+        <div class="player-score">0</div>
+        <img src="./assets/images/player-one.svg" alt="" class="player-icon" />
+      </div>
+    `;
+
+      rightScoreboard.innerHTML = `
+      <div class="player-box box-shadow-black">
+        <div class="player-name">CPU</div>
+        <div class="player-score">0</div>
+        <img src="./assets/images/cpu-icon.svg" alt="" class="player-icon" />
+      </div>
+    `;
+    }
+  };
+
   const bindEvents = () => {
-    pvpButton.addEventListener("click", showGameScreen);
+    document.getElementById("pvp-btn").addEventListener("click", () => {
+      updateScoreboard("pvp");
+      showGameScreen();
+    });
+
+    document.getElementById("cpu-btn").addEventListener("click", () => {
+      updateScoreboard("cpu");
+      showGameScreen();
+    });
   };
 
   return {

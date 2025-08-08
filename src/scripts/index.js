30385ebedea0dbf948f1a89419a2b982cafe1dba
nickonyi <nickoluoch1130@gmail.com>
2025-08-08 22:44:07 +0300

    feat: implement menu controller for managing game rules and menu interactions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,14 @@
 import "../styles/style.css";
+import { MenuConroller } from "./menuController";
 
-const gameRulesBtn = document.querySelector("#game-rules-btn");
-const checkBtn = document.querySelector(".close-rules-btn");
-const menuBtn = document.querySelector(".menu-logo");
+const App = (() => {
+  const menu = MenuConroller();
 
-const toggleBoxes = () => {
-  const box1 = document.querySelector(".main-menu-tab");
-  const box2 = document.querySelector(".rules-section");
+  const start = () => {
+    menu.init();
+  };
 
-  if (box1.classList.contains("visible")) {
-    box1.classList.replace("visible", "hidden");
-    box2.classList.replace("hidden", "visible");
-  } else {
-    box2.classList.replace("visible", "hidden");
-    box1.classList.replace("hidden", "visible");
-  }
-};
+  return { start };
+})();
 
-const bringMenu = () => {
-  document.querySelector(".modal").style.display = "flex";
-};
-
-gameRulesBtn.addEventListener("click", toggleBoxes);
-menuBtn.addEventListener("click", bringMenu);
-checkBtn.addEventListener("click", toggleBoxes);
+App.start();
---
+++ src/scripts/menuController.js
@@ -0,0 +1,33 @@
+export const MenuConroller = () => {
+  const gameRulesBtn = document.querySelector("#game-rules-btn");
+  const checkBtn = document.querySelector(".close-rules-btn");
+  const menuBtn = document.querySelector(".menu-logo");
+
+  const toggleBoxes = () => {
+    const box1 = document.querySelector(".main-menu-tab");
+    const box2 = document.querySelector(".rules-section");
+
+    if (box1.classList.contains("visible")) {
+      box1.classList.replace("visible", "hidden");
+      box2.classList.replace("hidden", "visible");
+    } else {
+      box2.classList.replace("visible", "hidden");
+      box1.classList.replace("hidden", "visible");
+    }
+  };
+  const bringMenu = () => {
+    document.querySelector(".modal").style.display = "flex";
+  };
+
+  const bindEvents = () => {
+    gameRulesBtn.addEventListener("click", toggleBoxes);
+    menuBtn.addEventListener("click", bringMenu);
+    checkBtn.addEventListener("click", toggleBoxes);
+  };
+
+  return {
+    init: () => {
+      bindEvents();
+    },
+  };
+};

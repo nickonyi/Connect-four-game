6f3771fb3088672fc92301959a34fa170a5c1eb2
nickonyi <nickoluoch1130@gmail.com>
2025-08-11 18:15:40 +0300

    feat: update menu functionality and improve button interactions for better user experience

--- src/scripts/menuController.js
+++ src/scripts/menuController.js
@@ -1,7 +1,10 @@
 export const MenuConroller = () => {
   const gameRulesBtn = document.querySelector("#game-rules-btn");
   const checkBtn = document.querySelector(".close-rules-btn");
-  const menuBtn = document.querySelector(".menu-logo");
+  const menuBtn = document.querySelector(".game-info-menu");
+  const continueBtn = document.getElementById("resume-btn");
+  const restartBtn = document.getElementById("restart-btn");
+  const quitBtn = document.getElementById("quit-btn");
 
   const toggleBoxes = () => {
     const box1 = document.querySelector(".main-menu-tab");
@@ -15,14 +18,27 @@ export const MenuConroller = () => {
       box1.classList.replace("hidden", "visible");
     }
   };
+
+  const close = () => {
+    document.querySelector(".modal").style.display = "none";
+  };
+
   const bringMenu = () => {
     document.querySelector(".modal").style.display = "flex";
+  };
+
+  const quitGame = () => {
+    close();
+    document.querySelector("#game-screen").classList.add("hidden");
+    document.querySelector("#home-screen").classList.remove("hidden");
   };
 
   const bindEvents = () => {
     gameRulesBtn.addEventListener("click", toggleBoxes);
     menuBtn.addEventListener("click", bringMenu);
     checkBtn.addEventListener("click", toggleBoxes);
+    continueBtn.addEventListener("click", close);
+    quitBtn.addEventListener("click", quitGame);
   };
 
   return {
